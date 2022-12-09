9654dc20b67a0fd8b9db68757aae352fc774e184
KSUSHAPhil <philkins2014@mail.ru>
2022-12-09 20:44:11 +0300

    added generateDate

--- src/test/java/CardTest.java
+++ src/test/java/CardTest.java
@@ -2,19 +2,30 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.Test;
 
 import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.*;
 
 public class CardTest {
+
+    public String generateDate(int days) {
+        return
+                LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
+
     @Test
-    void Testik() {
+    void happyDelivery() {
         Configuration.holdBrowserOpen = true;
+
+        String planningDate = generateDate(4);
 
         open("http://localhost:9999");
         $("[data-test-id ='city'] input").val("Казань");
         $("[data-test-id = 'name'] input").val("Ксюшенька");
         $("[data-test-id = 'phone'] input").val("+79995558978");
+        $("[data-test-id = 'date'] input").val(planningDate);
         $("[data-test-id = 'agreement']").click();
         $x("//span[@class='button__text']").click();
         $("[data-test-id = 'notification']").shouldBe(visible, Duration.ofSeconds(15));
